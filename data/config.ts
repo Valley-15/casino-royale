cf4106f3b14a23e7d66dd9282bacb1411f7a9f5a
Adnaan <adnaan.2735@gmail.com>
2025-06-21 22:44:37 +0530

    feat: add bio and skills to siteConfig

--- data/config.ts
+++ data/config.ts
@@ -3,9 +3,19 @@
 export const siteConfig = {
   name: "Adnaan Ali",
   tagline: "Experienced Web Developer",
+  bio: `I write code not just to build, but to express. I’m Adnaan, a web developer chasing beauty in both function and freedom, in the spirit of open source.`,
+  skills: [
+    "MongoDB",
+    "Express.js",
+    "React.js",
+    "Node.js",
+    "Next.js",
+    "Tailwind CSS",
+    "Firebase",
+  ],
   cta: {
     text: "View Projects",
-    href: "#projects", // scroll target: <section id="projects">
+    href: "#projects",
   },
   email: "adnaan.2735@gmail.com",
   socialLinks: {
